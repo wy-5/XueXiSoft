01351558ad2588738c8ae5b69b611326d66a031f
wuyong <1836576324@qq.com>
2022-11-18 19:20:54 +0800

    first

---
+++ XueXiSoft/app/src/main/java/com/example/lanhong/XueXiSoft/order_talkActivity.java
@@ -0,0 +1,16 @@
+package com.example.lanhong.XueXiSoft;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class order_talkActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.mes_order_talk);
+    }
+
+}
